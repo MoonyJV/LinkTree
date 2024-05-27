96ba84e8e266e5f29f2d3eff7629142099669395
MoonyJV <joaovictorvasconcelos27@gmail.com>
2024-05-27 01:27:18 -0300

    commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function togglemode() {
+    document.body.classList.remove('insta');
+    document.body.classList.remove('batma');
+    document.body.classList.toggle('light');
+}
+
+function secreto() {
+    document.body.classList.remove('batma');
+    document.body.classList.toggle('insta')
+}
+
+function dc() {
+    document.body.classList.remove('insta');
+    document.body.classList.toggle('batma')
+}
